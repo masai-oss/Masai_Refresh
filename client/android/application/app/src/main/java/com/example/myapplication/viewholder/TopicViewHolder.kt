68624e3ff99c1550675c416a1b5f41e9fdc48034
Abhinav Shukla <abhinav2014shukla@gmail.com>
2021-03-09 01:11:23 +0530

    Added sample icons for topics

--- client/android/application/app/src/main/java/com/example/myapplication/viewholder/TopicViewHolder.kt
+++ client/android/application/app/src/main/java/com/example/myapplication/viewholder/TopicViewHolder.kt
@@ -11,8 +11,8 @@ class TopicViewHolder(private val view:View):RecyclerView.ViewHolder(view) {
     val html="https://firebasestorage.googleapis.com/v0/b/splitwise-project.appspot.com/o/Html.PNG?alt=media&token=b992f284-c852-4797-a9b7-5033f27319f4"
     val ds="https://firebasestorage.googleapis.com/v0/b/splitwise-project.appspot.com/o/Ds.png?alt=media&token=61902161-4f03-4fd9-b17e-a01d8095448a"
     val react="https://firebasestorage.googleapis.com/v0/b/splitwise-project.appspot.com/o/React.JPG?alt=media&token=569b4a08-26da-4933-a4aa-1ecba1d5ad4e"
-    val nodejs="https://firebasestorage.googleapis.com/v0/b/splitwise-project.appspot.com/o/nodejs.svg?alt=media&token=43bceeda-7f1a-4004-8475-eba6cb75b284"
-    val express="https://firebasestorage.googleapis.com/v0/b/splitwise-project.appspot.com/o/expressjs-ar21.svg?alt=media&token=00c2052c-3629-443b-89b6-ab0ea70ee14c"
+    val nodejs="https://firebasestorage.googleapis.com/v0/b/splitwise-project.appspot.com/o/download.png?alt=media&token=23c3cad9-5ef6-4b70-8129-791b406b2981"
+    val express="https://firebasestorage.googleapis.com/v0/b/splitwise-project.appspot.com/o/Expressjs.png?alt=media&token=74fc7217-fa8a-4a52-b1ff-f12668bc1d93"
 
     fun setData(dataItem: DataItem){
         view.apply {
