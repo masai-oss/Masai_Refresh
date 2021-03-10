db2ba79717b30bfa709b195ffd9916d2dbc314a6
vchandu111 <vchanduu111@gmail.com>
2021-03-10 21:21:10 +0530

    displayingQuestionsnew

--- client/web/src/Structure/Topics/Components/Topics.js
+++ client/web/src/Structure/Topics/Components/Topics.js
@@ -1,24 +1,9 @@
 import React,{useEffect, useState} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getTopics } from '../State/action'
-import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
-import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: 'absolute',
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
 
 function Topics() {
-    const classes = useStyles();
 
     const dispatch = useDispatch()
 
@@ -26,39 +11,23 @@ function Topics() {
 useEffect(() => {
    dispatch(getTopics())
 }, [])
-    const { topicsData } = useSelector(state => state.topics)
+  
+  
+  const { topicsData } = useSelector(state => state.topics)
+  
+  console.log(topicsData)
 
-    const [openModal, setOpenModal] = useState(false)
 
-    const [topic, setTopic] = useState({})
-
-    const startQuiz = (topicId) => {
-        setOpenModal(true)
-        let selectedTopic = topicsData.find(topic => topic.id == topicId)
-        setTopic(selectedTopic)
-    }
-
-  const handleClose = () => {
-      setOpenModal(false)
-    }
 
   return 
   <div style={{ display: 'flex', flex: 2, justifyContent: 'space-around' }}>
-        {topicsData.map(topic => <div key={topic.id}>
+        {topicsData?.map(topic => <div key={topic.id}>
             <h3>{topic.name}</h3>
             <h6>Total Questions: {topic.total_questions}</h6>
-          <button onClick={() => startQuiz(topic.id)}>Attempt</button>
-          {/* <Modal
-        open={openModal}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        {topic}
-      </Modal> */}
+          <button >Attempt</button>
+          
         </div>)}
       
-    {openModal && 'hi'}
     </div>
 }
 
