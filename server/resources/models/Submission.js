7bda0731eb60d04dc0b8b8a78555221570b4fa6d
AravindSakthivel <aravind.sakthi@outlook.com>
2021-03-10 14:28:43 +0530

    Added Reuslts API

--- server/resources/models/Submission.js
+++ server/resources/models/Submission.js
@@ -1,107 +1,111 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const outcomeEnum = require('../utils/enums/OutcomeEnum')
+const outcomeEnum = require("../utils/enums/OutcomeEnum");
 const QuestionTypeEnum = require("../utils/enums/QuestionTypeEnum");
 
 const submissionSchema = new Schema(
-    {
-        topic_id: {
-            required: true,
-            type: Schema.Types.ObjectId
-        },
-        user_id: {
-            required: true,
-            type: Schema.Types.ObjectId
-        },
-        stats: {
-            alloted: {
-                type: Number,
-                default: 0
-            },
-            skipped: {
-                type: Number,
-                default: 0
-            },
-            correct: {
-                type: Number,
-                default: 0
-            },
-            wrong: {
-                type: Number,
-                default: 0
-            }
-        },
-        attempts: [
-            {
-                questions: [Schema.Types.ObjectId],
-                answers: [
-                    {
-                        question_id: {
-                            type: Schema.Types.ObjectId,
-                            required: true
-                        },
-                        type: {
-                          type: String,
-                          required: true,
-                          enum: [...Object.values(QuestionTypeEnum)],
-                        },
-                        outcome: {
-                            type: String,
-                            default: outcomeEnum.SKIPPED,
-                            enum: [...Object.values(outcomeEnum)]
-                        },
-                        time: {
-                            required: true,
-                            type: String
-                        },
-                        response: {
-                          type: Schema.Types.Mixed,
-                          required: function () {
-                            return this.type === QuestionTypeEnum.SHORT;
-                          }
-                        },
-                        selected: {
-                          type: Schema.Types.Mixed,
-                          required: function () {
-                            return this.type === QuestionTypeEnum.MCQ;
-                          }
-                        },
-                        decision: {
-                          type: Schema.Types.Mixed,
-                          required: function () {
-                            return this.type === QuestionTypeEnum.TF;
-                          }
-                        },
-                    }
-                ],
-                stats: {
-                    alloted: {
-                        type: Number,
-                        default: 0
-                    },
-                    skipped: {
-                        type: Number,
-                        default: 0
-                    },
-                    correct: {
-                        type: Number,
-                        default: 0
-                    },
-                    wrong: {
-                        type: Number,
-                        default: 0
-                    },
-                    time: {
-                        required: true,
-                        type: Number
-                    }
-                }
-            }
-        ]
+  {
+    topic_id: {
+      required: true,
+      type: Schema.Types.ObjectId,
     },
-    {
-        versionKey: false,
-    }
+    user_id: {
+      required: true,
+      type: Schema.Types.ObjectId,
+    },
+    stats: {
+      alloted: {
+        type: Number,
+        default: 0,
+      },
+      skipped: {
+        type: Number,
+        default: 0,
+      },
+      correct: {
+        type: Number,
+        default: 0,
+      },
+      wrong: {
+        type: Number,
+        default: 0,
+      },
+    },
+    attempts: [
+      {
+        questions: [Schema.Types.ObjectId],
+        isStatsUpdated: {
+          type: Boolean,
+          default: false
+        },
+        answers: [
+          {
+            question_id: {
+              type: Schema.Types.ObjectId,
+              required: true,
+            },
+            type: {
+              type: String,
+              required: true,
+              enum: [...Object.values(QuestionTypeEnum)],
+            },
+            outcome: {
+              type: String,
+              default: outcomeEnum.SKIPPED,
+              enum: [...Object.values(outcomeEnum)],
+            },
+            time: {
+              required: true,
+              type: String,
+            },
+            response: {
+              type: Schema.Types.Mixed,
+              required: function () {
+                return this.type === QuestionTypeEnum.SHORT;
+              },
+            },
+            selected: {
+              type: Schema.Types.Mixed,
+              required: function () {
+                return this.type === QuestionTypeEnum.MCQ;
+              },
+            },
+            decision: {
+              type: Schema.Types.Mixed,
+              required: function () {
+                return this.type === QuestionTypeEnum.TF;
+              },
+            },
+          },
+        ],
+        stats: {
+          alloted: {
+            type: Number,
+            default: 0,
+          },
+          skipped: {
+            type: Number,
+            default: 0,
+          },
+          correct: {
+            type: Number,
+            default: 0,
+          },
+          wrong: {
+            type: Number,
+            default: 0,
+          },
+          time: {
+            required: true,
+            type: Number,
+          },
+        },
+      },
+    ],
+  },
+  {
+    versionKey: false,
+  }
 );
 
 module.exports = mongoose.model("submission", submissionSchema);
