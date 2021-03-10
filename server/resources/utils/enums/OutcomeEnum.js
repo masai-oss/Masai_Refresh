186875817f20db17b47f3a112302b84bb56cd833
Arjun K Prasad <arjun1237.kp@gmail.com>
2021-03-10 15:52:39 +0530

    minor changes to enums

--- server/resources/utils/enums/OutcomeEnum.js
+++ server/resources/utils/enums/OutcomeEnum.js
@@ -1,8 +1,8 @@
 
 const outcome = {
-    "SKIPPED": "SKIPPED",
-    "CORRECT": "CORRECT",
-    "WRONG": "WRONG"
+    SKIPPED: "SKIPPED",
+    CORRECT: "CORRECT",
+    WRONG: "WRONG"
 }
 
 module.exports = Object.freeze(outcome)
--- server/resources/utils/enums/QuestionTypeEnum.js
+++ server/resources/utils/enums/QuestionTypeEnum.js
@@ -1,9 +1,9 @@
 
 const question_types = {
-    "TF" : "TF", // true false
-    "MCQ" : "MCQ", 
-    "SHORT" : "SHORT", // short answers
-    "LONG" : "LONG", // long answers
+    TF : "TF", // true false
+    MCQ : "MCQ", 
+    SHORT : "SHORT", // short answers
+    LONG : "LONG", // long answers
 }
 
 module.exports = Object.freeze(question_types)
