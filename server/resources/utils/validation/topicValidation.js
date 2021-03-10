5f8552a37d539e57993b5e027f037bf14a388a40
AravindSakthivel <aravind.sakthi@outlook.com>
2021-03-10 18:59:44 +0530

    Added Topic Validation

--- server/resources/utils/validation/topicValidation.js
+++ server/resources/utils/validation/topicValidation.js
@@ -1,1 +1,26 @@
 const Joi = require("joi");
+const topics = require("../enums/TopicsEnum");
+
+const topicValues = Object.values(topics);
+
+const topicValidation = (data) => {
+  const schema = Joi.object({
+    name: Joi.string()
+      .valid(...topicValues)
+      .required(),
+  });
+  return schema.validate(data);
+};
+
+
+const addTopicValidation = (data) => {
+  const schema = Joi.object({
+    name: Joi.string()
+      .valid(...topicValues)
+      .required(),
+    icon: Joi.string().required()
+  });
+  return schema.validate(data)
+}
+
+module.exports = { topicValidation, addTopicValidation }
