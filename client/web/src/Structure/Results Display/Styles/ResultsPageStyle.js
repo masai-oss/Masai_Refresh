6b660104cf86e5bd874a918607e425bd36a6a23e
vchandu111 <vchanduu111@gmail.com>
2021-03-16 12:18:59 +0530

    added explanation in results page

--- client/web/src/Structure/Results Display/Styles/ResultsPageStyle.js
+++ client/web/src/Structure/Results Display/Styles/ResultsPageStyle.js
@@ -5,15 +5,15 @@ export const ResultWrapper = styled.div`
 `;
 
 export const QuestionWrapper = styled.div`
-    border: 1px solid #e8e8e8;
-    padding: 10px 40px;
-    border-radius: 2px;
-    margin-bottom: 20px;
-    
-    :hover{
-        background-color: #f2f2f2;
-    }
-`
+  border: 1px solid #e8e8e8;
+  padding: 10px 40px;
+  border-radius: 2px;
+  margin-bottom: 20px;
+
+  :hover {
+    background-color: #f2f2f2;
+  }
+`;
 
 export const OutcomeTag = styled.span`
   background-color: ${(props) =>
@@ -86,3 +86,4 @@ export const Div = styled.div`
   margin-top: 30px;
   margin-bottom: 30px;
 `;
+
