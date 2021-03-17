28379976cf739fd865340c963c582afee09a6cab
Abhinav Shukla <abhinav2014shukla@gmail.com>
2021-03-17 12:15:49 +0530

    token changed

--- client/android/application/app/src/main/java/com/example/myapplication/repository/TopicsRepository.kt
+++ client/android/application/app/src/main/java/com/example/myapplication/repository/TopicsRepository.kt
@@ -8,7 +8,7 @@ class TopicsRepository (private val callback: retrofit2.Callback<TopicsModel>){
 
     fun getListOfTopics() {
         val apiClient=Network.getInstance().create(TopicApi::class.java)
-        val call=apiClient.getTopics("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ")
+        val call=apiClient.getTopics("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFiaGluYXZAbWFzYWlzY2hvb2wuY29tIiwiaWQiOiI2MDUxOWQ4NzlhMzY3NDAwMjJjMWZiYzkiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjE1OTYxNDc5LCJleHAiOjE2MTg1MTc0Nzl9.1AyMJ3jpE3ItWP9OamKVWEhH992YuesoEPJG_FofSAc")
         call.enqueue(callback)
 
     }
