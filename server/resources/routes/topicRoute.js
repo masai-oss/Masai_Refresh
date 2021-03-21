55b2d5bb90367fe650fa3df14ec8d58bccab0eba
Arjun K Prasad <arjun1237.kp@gmail.com>
2021-03-22 04:03:58 +0530

    changes to topic crud applied

--- server/resources/routes/topicRoute.js
+++ server/resources/routes/topicRoute.js
@@ -4,32 +4,20 @@ const {
   getTopicById,
   getTopicByName,
   addTopic,
+  updateIcon,
   deleteTopic,
-  editTopic,
 } = require("../controller/topicController");
 const { getTopicsSummary } = require("../controller/proficiencyController");
 const { authenticateToken } = require("../controller/authController");
 const { checkAdmin } = require("../utils/validation/adminValidation");
-const { upload } = require("../helper/uploader")
+const { upload } = require("../utils/helper/uploader")
 
 topicRoute.get("/", authenticateToken, checkAdmin, getAllTopics);
 topicRoute.get("/id/:id", authenticateToken, checkAdmin, getTopicById);
 topicRoute.get("/name/:name", authenticateToken, checkAdmin, getTopicByName);
 topicRoute.get("/summary", authenticateToken, getTopicsSummary);
-topicRoute.post(
-  "/create",
-  authenticateToken,
-  checkAdmin,
-  upload.single("icon"),
-  addTopic
-);
+topicRoute.post( "/create", authenticateToken, checkAdmin, upload.single("icon"), addTopic);
+topicRoute.patch( "/icon/:id", authenticateToken, checkAdmin, upload.single("icon"), updateIcon);
 topicRoute.delete("/:id", authenticateToken, checkAdmin, deleteTopic);
-topicRoute.patch(
-  "/:id",
-  authenticateToken,
-  checkAdmin,
-  upload.single("icon"),
-  editTopic
-);
 
 module.exports = topicRoute;
