2f0f03484ea6019c203cc3da6951338c4e79f14c
Manideep Pullalachervu <pmanideepnaidu@gmail.com>
2021-03-29 17:54:52 +0530

    added js data & js dump script

--- server/data/html/index.js
+++
@@ -1,9 +0,0 @@
-let sanfoundry = require("./sanfoundry.json").questions;
-let freeTimeLearning = require("./freeTimeLearning.json").questions;
-
-freeTimeLearning = freeTimeLearning.map((question) => ({
-  ...question,
-  source: "https://www.freetimelearning.com",
-}));
-
-module.exports = freeTimeLearning.concat(sanfoundry);
---
+++ server/dataDumpScript/dumpJS.js
@@ -0,0 +1,7 @@
+const dumpData = require("./dumpData.js");
+const jsData = require("./data/javascript/index.js");
+
+const filter = { _id: process.env.jsId };
+const update = { questions: jsData };
+
+dumpData(filter, update);
