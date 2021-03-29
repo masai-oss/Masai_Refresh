f2c97ab4c56a544ffff0fe452577b9efdb291980
AravindSakthivel <aravind.sakthi@outlook.com>
2021-03-29 12:28:10 +0530

    removed extra collection addition

--- server/resources/services/googleStrategy.js
+++ server/resources/services/googleStrategy.js
@@ -19,31 +19,42 @@ const googleStrategy = new GoogleStrategy(
   async (accessToken, refreshToken, profile, done) => {
     try {
       const { id, displayName, emails, photos } = profile;
-      let role =
-        emails[0].value.split("@")[1] === ADMIN_CONTROL_EMAIL
-          ? "admin"
-          : "user";
-      const currentUser = await User.findOne({
-        oauth: { $elemMatch: { provider: "google", identifier: id } },
-      });
-      if (!currentUser) {
-        const newUser = await new User({
-          name: displayName,
-          email: emails[0].value,
-          role: role,
-          profilePic: photos[0].value,
-          oauth: [
-            {
-              provider: "google",
-              identifier: id,
-            },
-          ],
-        }).save();
-        if (newUser) {
-          return done(null, newUser);
+      let oauth = { provider: "google", identifier: id };
+      const userPresentWithGmail = await User.findOne({ email: emails[0].value });
+      if (!userPresentWithGmail) {
+        const role =
+          emails[0].value.split("@")[1] === ADMIN_CONTROL_EMAIL
+            ? "admin"
+            : "user";
+        const currentUser = await User.findOne({
+          oauth: { $elemMatch: { provider: "google", identifier: id } },
+        });
+        if (!currentUser) {
+          const newUser = await new User({
+            name: displayName,
+            email: emails[0].value,
+            role: role,
+            profilePic: photos[0].value,
+            oauth: [oauth],
+          }).save();
+          if (newUser) {
+            return done(null, newUser);
+          }
         }
+        return done(null, currentUser);
+      } else if (
+        userPresentWithGmail.oauth.length === 2 ||
+        userPresentWithGmail.oauth[0].provider === "google"
+      ) {
+        return done(null, userPresentWithGmail);
+      } else {
+        const modifiedUser = await User.findOneAndUpdate(
+          { email: emails[0].value },
+          { $push: { oauth: oauth } },
+          { returnOriginal: false }
+        );
+        return done(null, modifiedUser);
       }
-      done(null, currentUser);
     } catch (err) {
       done(new Error(`Failed ${err}`));
     }
