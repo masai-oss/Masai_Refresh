613fab3d4a278b14e0e9a7979680e35d9e9777e5
Nitansh <nitansh11@gmail.com>
2021-06-11 21:47:48 +0530

    Long Questions Bug resolved

--- client/web/src/Structure/Practice/Components/LongType.jsx
+++ client/web/src/Structure/Practice/Components/LongType.jsx
@@ -38,7 +38,7 @@ const LongType = () => {
 
   const dispatch = useDispatch();
   const history = useHistory();
-  const { statement, answer, like_flag, bookmark_flag, likes } = question;
+  // const { statement, answer, like_flag, bookmark_flag, likes } = question;
 
   const getQuestion = (index) => {
     history.push(`${index}`);
@@ -64,70 +64,77 @@ const LongType = () => {
       })
     );
   };
+  console.log(question);
 
   return isLoading ? (
     <Spinner />
   ) : (
     <>
-      <div className={styles.question}>
-        <p className={styles.queFont}>{statement}</p>
-        <div className={styles.icons}>
-          {bookmark_flag ? (
-            <BookmarkIcon
-              className={styles.filledBookmark}
-              onClick={toggleBookmark}
-            ></BookmarkIcon>
-          ) : (
-            <BookmarkBorderIcon
-              className={styles.bookmark}
-              onClick={toggleBookmark}
-            ></BookmarkBorderIcon>
-          )}
-          <div className={styles.likesDiv}>
-            {like_flag ? (
-              <FavoriteIcon
-                onClick={toggleLike}
-                className={styles.filledHeart}
-              ></FavoriteIcon>
-            ) : (
-              <FavoriteBorderOutlined
-                onClick={toggleLike}
-                className={styles.heart}
-              ></FavoriteBorderOutlined>
-            )}
-            <div className={styles.likes}>{likes}</div>
+      {question ? (
+        <div>
+          <div className={styles.question}>
+            <p className={styles.queFont}>{question.statement}</p>
+            <div className={styles.icons}>
+              {question.bookmark_flag ? (
+                <BookmarkIcon
+                  className={styles.filledBookmark}
+                  onClick={toggleBookmark}
+                ></BookmarkIcon>
+              ) : (
+                <BookmarkBorderIcon
+                  className={styles.bookmark}
+                  onClick={toggleBookmark}
+                ></BookmarkBorderIcon>
+              )}
+              <div className={styles.likesDiv}>
+                {question.like_flag ? (
+                  <FavoriteIcon
+                    onClick={toggleLike}
+                    className={styles.filledHeart}
+                  ></FavoriteIcon>
+                ) : (
+                  <FavoriteBorderOutlined
+                    onClick={toggleLike}
+                    className={styles.heart}
+                  ></FavoriteBorderOutlined>
+                )}
+                <div className={styles.likes}>{question.likes}</div>
+              </div>
+            </div>
           </div>
+          <div className={styles.answer}>
+            <ReactMarkdown
+              className={styles.answerFont}
+              renderers={{ code: SyntaxHighlight }}
+            >
+              {question.answer}
+            </ReactMarkdown>
+            <div className={styles.hr}></div>
+            <div style={{ display: "flex" }}>
+              <img className={styles.img} src={report} alt="report" />
+              <span className={styles.report}>Report an issue</span>{" "}
+            </div>
+          </div>
+          <div className={styles.nextBtn}>
+            <button
+              disabled={indexNum == 1 ? true : false}
+              onClick={() => getQuestion(indexNum - 1)}
+              className={indexNum == 1 ? styles.disabledBtn : styles.btn}
+            >
+              Back
+            </button>
+            <button
+              onClick={() => getQuestion(indexNum + 1)}
+              className={styles.btn}
+            >
+              Next
+            </button>
+          </div>
+          <div style={{ height: 100 }}></div>
         </div>
-      </div>
-      <div className={styles.answer}>
-        <ReactMarkdown
-          className={styles.answerFont}
-          renderers={{ code: SyntaxHighlight }}
-        >
-          {answer}
-        </ReactMarkdown>
-        <div className={styles.hr}></div>
-        <div style={{ display: "flex" }}>
-          <img className={styles.img} src={report} alt="report" />
-          <span className={styles.report}>Report an issue</span>{" "}
-        </div>
-      </div>
-      <div className={styles.nextBtn}>
-        <button
-          disabled={indexNum == 1 ? true : false}
-          onClick={() => getQuestion(indexNum - 1)}
-          className={indexNum == 1 ? styles.disabledBtn : styles.btn}
-        >
-          Back
-        </button>
-        <button
-          onClick={() => getQuestion(indexNum + 1)}
-          className={styles.btn}
-        >
-          Next
-        </button>
-      </div>
-      <div style={{ height: 100 }}></div>
+      ) : (
+        <Spinner />
+      )}
     </>
   );
 };
