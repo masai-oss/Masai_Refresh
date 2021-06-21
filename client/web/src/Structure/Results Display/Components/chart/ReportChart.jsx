5985fdaa8b2ff168a74c0f989e558f2fca6ab14a
kumarrohan2804 <kumarrohan2804@gmail.com>
2021-06-21 11:59:54 +0530

    adding back to take a new test button

--- client/web/src/Structure/Results Display/Components/chart/ReportChart.jsx
+++ client/web/src/Structure/Results Display/Components/chart/ReportChart.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import StyleContainer from "../utils/StyleContainer";
 import styles from "../../Styles/ReportChart.module.css";
 import ReportGraph from "./ReportGraph";
+import { useHistory } from "react-router-dom";
 
 const ReportChart = (props) => {
+  let history = useHistory();
+  const goBackToHome = () => {
+    history.replace("quiz_topics");
+  };
   return (
     <StyleContainer className={styles.reportchart__container}>
       <div className={styles.reportchart__totalquestion}>5</div>
@@ -11,7 +16,10 @@ const ReportChart = (props) => {
       <hr className={styles.reporttotalquestion__hr} />
       <ReportGraph chart_value={props.chart_value} />
       <div className={styles.reportchart__testbutton}>
-        <div className={styles.reportchart__testbutton_title}>
+        <div
+          className={styles.reportchart__testbutton_title}
+          onClick={goBackToHome}
+        >
           Take a new test
         </div>
       </div>
