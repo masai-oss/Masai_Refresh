2d0ccf45981598e2e50fdcdd78181bbb780a9b02
Nitansh <nitansh11@gmail.com>
2021-06-30 09:54:24 +0530

    isAdmin resolved

--- client/web/src/Structure/Common/hooks/IsAdmin.jsx
+++ client/web/src/Structure/Common/hooks/IsAdmin.jsx
@@ -2,13 +2,13 @@ import jwt_decode from "jwt-decode";
 import { getFromStorage } from "../../../Utils/localStorageHelper";
 import { storageEnums } from "../../../Enums/storageEnums";
 
-
 const IsAdmin = () => {
   const token = getFromStorage(storageEnums.TOKEN, null);
   if (token === null) {
-    return false
+    return false;
   }
   const { admin: isAdmin } = jwt_decode(token);
+  console.log("Value to admin is: ", isAdmin);
   return isAdmin;
 };
 
