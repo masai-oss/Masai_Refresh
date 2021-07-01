80b73dc489ba1c6437ac2ff470ed07ff1d698a2c
Nitansh <nitansh11@gmail.com>
2021-07-01 19:31:56 +0530

    Minor css changes in Complete practice page

--- client/web/src/Structure/Practice/Components/Completed.jsx
+++ client/web/src/Structure/Practice/Components/Completed.jsx
@@ -18,25 +18,51 @@ const Completed = () => {
   const goToPractice = () => {
     history.push("/practice_topics");
   };
+  let name = getFromStorage(storageEnums.LONG_TYPE_NAV_TOPIC, "");
+  const logoPath = `/logos/${name.toLowerCase()}/${name.toLowerCase()}_logo.svg`;
   return isLoading ? (
     <Spinner />
   ) : (
     <>
-      <div className={styles.outer}>
-        <div className={styles.stars}>
-          <img src={star} alt="star" />
+      <div className={styles.completed__Outer}>
+        <div className={styles.bookmarkNavbar}>
+          <div>
+            <img src="/logos/WhiteLessThanIcon.svg" alt="close icon" />
+            <p
+              onClick={() => {
+                history.push("/");
+              }}
+            >
+              Go Back To Homepage
+            </p>
+          </div>
+          <div className={styles.boomarkNavbarLogo}>
+            <img src={logoPath} alt="close icon" />
+            <p
+              onClick={() => {
+                // history.push(`/bookmarks/${topic_ID}`);
+              }}
+            >
+              {name}
+            </p>
+          </div>
         </div>
-        <div className={styles.message}>
-          You’ve completed{" "}
-          {getFromStorage(storageEnums.LONG_TYPE_NAV_TOPIC, "")} Practice
-        </div>
-        <div>
-          <button onClick={goToPractice} className={styles.btn}>
-            Practice again
-          </button>
-        </div>
-        <div className={styles.vector}>
-          <img src={boy} alt="boy" />
+        <div className={styles.completed__Parent}>
+          <div className={styles.completed__Star}>
+            <img src={star} alt="star" />
+          </div>
+          <div className={styles.completed__Message}>
+            You’ve completed{" "}
+            {getFromStorage(storageEnums.LONG_TYPE_NAV_TOPIC, "")} Practice
+          </div>
+          <div>
+            <button onClick={goToPractice} className={styles.completed__Button}>
+              Practice again
+            </button>
+          </div>
+          <div className={styles.completed__Vector}>
+            <img src={boy} alt="boy" />
+          </div>
         </div>
       </div>
     </>
